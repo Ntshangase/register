239aace2d369f0dafee451a003732bdff1e9d206
Simphiwe Ntshangase <simphiwe323i@gmail.com>
2024-04-16 07:01:28 +0200

    Local host running with GetMapping

--- src/main/java/com/mntwana/register/RegisterApplication.java
+++ src/main/java/com/mntwana/register/RegisterApplication.java
@@ -2,9 +2,22 @@ package com.mntwana.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RegisterApplication {
+
+	@GetMapping("/")
+	public String onStart() {
+		return "Welcome to my Spring-boot Application";
+	}
+
+	@GetMapping("/home")
+	public String Greet() {
+		return "Hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterApplication.class, args);
