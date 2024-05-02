46ef7ce3462432dc8f8f2f6a37d9c65c767acb37
Simphiwe Ntshangase <simphiwe323i@gmail.com>
2024-05-02 07:33:56 +0200

    Cross Origin Request Sharing

--- src/main/java/com/mntwana/register/Students.java
+++ src/main/java/com/mntwana/register/Students.java
@@ -1,8 +1,10 @@
 package com.mntwana.register;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 public class Students {
     
@@ -46,6 +48,6 @@ public class Students {
         users.setCourse("Information Technology");
         users.setStudentId(22232239);
 
-        return "name: "+ users.name + "  surname: "+ users.surname + "  course: "+ users.course + "  studentId: "+ users.studentId;
+        return users.name + users.surname + users.course + users.studentId;
     }
 }
