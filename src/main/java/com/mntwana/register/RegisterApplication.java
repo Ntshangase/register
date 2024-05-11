8f51141e138630c38afc1767fae1159ce2078dfb
Simphiwe Ntshangase <simphiwe323i@gmail.com>
2024-05-11 08:02:15 +0200

    Application Programming Interface

--- src/main/java/com/mntwana/register/RegisterApplication.java
+++ src/main/java/com/mntwana/register/RegisterApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @SpringBootApplication
@@ -26,11 +27,19 @@ public class RegisterApplication {
 
 		// instantiate object
 		Students student = new Students("Simphiwe", "Ntshangase","ICT", 3576);
+		Students student2 = new Students("Nas", "Greatest", "Marketing", 2022);
+		Students student3 = new Students("Nkazimulo", "Jesus", "ICT", 2732);
 
 		// map objects with key:value pairs.
-		//ObjectMapping mapping = new ObjectMapping();
+		ObjectMapper mapper = new ObjectMapper();
 
-		return "hey";
+		try{
+			String json = mapper.writeValueAsString(student);
+			return json;
+		} catch(Exception e){
+			return e.getMessage();
+		}
+
 	}
 
 	public static void main(String[] args) {
